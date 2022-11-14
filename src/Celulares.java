cbb12d8bc4716823503146e1f98abd61fa414d01
Daniel <daniel.quishpe@epn.edu.ec>
2022-11-13 19:39:48 -0500

    Deber finalizado

---
+++ src/Celulares.java
@@ -0,0 +1,43 @@
+public class Celulares {
+    String marca= Sony;
+    int anio=2022;
+    String = verde;
+    int camaras= 4;
+
+    public celulares() {
+    }
+    public void  abrir-aplicacion(){
+    }
+    public void cerrar-aplicacion(){
+    }
+    public void  llamar(){
+    }
+    public void colgar-llamada(){
+    }
+    public void reproducir-musica(){
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getCamaras() {
+        return camaras;
+    }
+
+    public void setCamaras(int camaras) {
+        this.camaras = camaras;
+    }
+}
